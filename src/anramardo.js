c34b0097ca21f06d95149160c892653985652135
anrainie <anrainie@163.com>
2017-09-11 22:14:12 +0800

    感觉要推翻重做

--- src/anramardo.js
+++ src/anramardo.js
@@ -15,17 +15,15 @@ anramardo.HtmlReader = function () {
     let status = 0;
 
     let read = function (rows) {
-        let reader;
+        let para;
         let r = '';
         for (let i = 0, len = rows.length; i < len; i++) {
-            let row = rows[i];
-            if (reader == null)
-                reader = anramardo.ParagraphReader.create(row);
-            else
-                reader = reader.next(row);
-            if (reader instanceof anramardo.ParagraphEnd) {
-                r += reader.get();
-                reader = null;
+            if (para == null)
+                para = new anramardo.Paragraph();
+            para = para.next(rows[i]);
+            if (para.isEnd) {
+                r += para.get();
+                para = null;
             }
 
         }
@@ -38,39 +36,115 @@ anramardo.HtmlReader = function () {
     };
 };
 
-anramardo.ParagraphReader = {
-    create(row){
-        return new anramardo.Start().next(row);
-    }
-};
-
-anramardo.Start=function(){
-    this.get=function(){return ''};
-    this.next=function(row){
-
-    };
-    return this;
-};
-anramardo.NormalReader = function (prev) {
-    this.prev = prev;
+anramardo.__get = function () {
+    if (this.prev == null)return this.content;
+    return this.prev.get() + '' + this.content;
+}
+/**
+ * 开始段落，需要分析整个根的类型
+ * @returns {anramardo.Paragraph}
+ * @constructor
+ */
+anramardo.Paragraph = function () {
+    let _self = this;
+    this.content = '';
+    this.get = anramardo.__get;
     this.next = function (row) {
+        //检查是否为结束段落
+        let answer;
+        if (_self.checkEnd)
+            answer = _self.checkEnd(row);
 
+        let rr = _self.handler?_self.handler:Handlers[row.charAt(0)];
+        if (rr == null)
+            rr = Handlers.NORMAL;
+        let result = rr(row);
 
+        if (result.checkEnd||(answer && !answer.end)) {
+            _self.content = result.content;
 
-        return new anramardo.ParagraphEnd(this);
-    };
-    this.get = function () {
-        return prev.get();
+            let next = new anramardo.Paragraph();
+            next.prev = _self;
+            next.checkEnd = result.checkEnd;
+            next.handler=result.nextHandler;
+            console.log(_self);
+            return next;
+        } else {
+            if (answer && answer.finish)
+                _self.content = answer.finish(result.content);
+            else
+                _self.content = result.content;
+
+            console.log(_self.content)
+            _self.isEnd = true;
+            return _self;
+        }
     };
     return this;
 };
-anramardo.ParagraphEnd = function (prev) {
-    this.prev = prev;
-    this.next = function () {
-        return null;
-    };
-    this.get = function () {
-        return prev.get();
-    };
-    return this;
-};
+
+let Handlers = {
+    '#'(s){
+        let count = 0;
+        while (s.charAt(count++) == '#' && count < 6) {
+        }
+        let h = 'h' + (count-1);
+        return {
+            content: '<' + h + '>' + s.substr(count - 1) + '</' + h + '>',
+            checked: true,
+        };
+    },
+    '-'(s, c = '-'){
+        let count = 0, space = 0;
+        while (true) {
+            let cr = s.charAt(count + space);
+            if (cr == c)
+                count++;
+            else if (cr == ' ') space++;
+            else break;
+            if (count > 2)
+                return {
+                    content: '<HR>',
+                    checked: true,
+                }
+        }
+        return Handlers.NORMAL(s);
+    },
+    '*'(s){
+        return Handlers['-'](s, "*");
+    },
+    ' '(s){
+        let count = 0;
+        while (s.charAt(count++) == ' ') {
+            if (count > 3) {
+                let n = s.charAt(count + 1);
+                return {
+                    content: '<pre><code>' + s.substr(count),
+                    nextHandler(s){
+                        return s+'\n';
+                    },
+                    checkEnd: function (s) {
+                        let count = 0;
+                        while (s.charAt(count++) == ' ') {
+                        }
+                        if (count < 4)
+                            return {
+                                end: true,
+                                finish(s){
+                                    '</code></pre>' + s
+                                }
+                            }
+
+                    }
+                }
+            }
+        }
+        return Handlers.NORMAL(s);
+    },
+    NORMAL(r){
+        return {
+            content: '<p>' + r + '</p>',
+            checked: false,
+        }
+    }
+}
